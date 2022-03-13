f976449288605abc5e49ace020760b4e48d65f83
varna <rylune@gmail.com>
2022-03-13 01:36:57 -0500

    1.1.0

--- EnhancedOSP/EnhancedOSP.cs
+++ EnhancedOSP/EnhancedOSP.cs
@@ -1,27 +1,58 @@
 ﻿using BepInEx;
 using BepInEx.Configuration;
 using IL.RoR2;
+using Mono.Cecil.Cil;
 using MonoMod.Cil;
+using System;
+using UnityEngine;
 
 namespace EnhancedOSP
 {
-    [BepInPlugin("com.Varna.EnhancedOSP", "EnhancedOSP", "1.0.1")]
+    [BepInPlugin("com.Varna.EnhancedOSP", "EnhancedOSP", "1.1.0")]
     public class EnhancedOSP : BaseUnityPlugin
     {
         private static ConfigEntry<float> invTime;
+        private static ConfigEntry<float> invThreshold;
+        private static ConfigEntry<bool> curseAffectsOSP;
 
         public void Awake()
         {
             invTime = Config.Bind("", "Invulnerable Time", 0.5f, new ConfigDescription("The amount of time a player remains invulnerable after one shot protection is triggered. Vanilla is 0.1"));
             HealthComponent.TriggerOneShotProtection += (il) =>
             {
-                var c = new ILCursor(il);
+                ILCursor c = new ILCursor(il);
                 c.GotoNext(
                     x => x.MatchLdarg(0),
                     x => x.MatchLdcR4(0.1f)
                     );
-                c.Index += 1;
+                c.Index++;
                 c.Next.Operand = invTime.Value;
+            };
+            Logger.LogDebug("Invuln time successful");
+            
+            invThreshold = Config.Bind("", "OSP Threshold", 0.1f, new ConfigDescription("How much missing hp% before OSP is disabled. Vanilla is 0.1"));
+            curseAffectsOSP = Config.Bind("", "Curse Behavior", false, new ConfigDescription("Whether or not sources of MaxHP reduction (Shaped Glass, etc) remove OSP. Vanilla is true"));
+            CharacterBody.RecalculateStats += (il) =>
+            {
+
+                ILCursor c = new ILCursor(il);
+                c.GotoNext(
+                    x => x.MatchLdarg(0),
+                    x => x.MatchLdcR4(0.1f),
+                    x => x.MatchCallOrCallvirt<RoR2.CharacterBody>("set_oneShotProtectionFraction")
+                    );
+                c.Index++;
+                c.Next.Operand = invThreshold.Value;
+                Logger.LogDebug("OSP threshold successful");
+
+                c.GotoNext(
+                    x => x.MatchCallOrCallvirt<Mathf>("Max"),
+                    x => x.MatchCallOrCallvirt<RoR2.CharacterBody>("set_oneShotProtectionFraction")
+                    );
+                c.Index++;
+                c.Emit(OpCodes.Ldarg_0);
+                c.EmitDelegate<Func<float, RoR2.CharacterBody, float>>((origFrac, body) => { return curseAffectsOSP.Value ?  origFrac : body.oneShotProtectionFraction; });
+                Logger.LogDebug("Curse behavior successful");
             };
         }
     }
--- EnhancedOSP/README.md
+++ EnhancedOSP/README.md
@@ -1,14 +1,20 @@
 # Enhanced OSP (One Shot Protection)
 
-A simple mod that allows you to set the invincibility window granted by OSP. (default setting is 0.5s, Vanilla is 0.1s)
+A simple mod that allows you to:
+
+- Set the invincibility window granted by OSP. (default setting is 0.5s, Vanilla is 0.1s)
+- Set the threshold of missing HP required before a player loses OSP. (default is 0.1, or 10% missing HP, same as Vanilla)
+- Set whether sources of maxHP reduction such as Shaped Glass negatively impact OSP. (default is false, Vanilla is true)
 
 You can reach me (Varna) in the [RoR2 modding discord](https://discord.gg/5MbXZvd) with any feedback!
 
-## Todo (maybe)
+## Credits
 
-- Settings for changing the health threshold amount, as well as for the behavior with maxHP reduction from glass/tonic stacks.
+[ThinkInvis](https://github.com/ThinkInvis/RoR2-TinkersSatchel) - For the location of the curse fraction setting + code I based my settings on.
 
 ## Changelog
+
+1.1.0 - Added two settings - one to control OSP threshold, and one to control whether maxHP reduction affects OSP (default false).
 
 1.0.1 - Updated to work with SotV expansion. Removed R2API dependency.
 

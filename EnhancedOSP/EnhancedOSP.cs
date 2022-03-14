149e6dacf9fac63e0a2d05a8fc004ee10e2dbac6
varna <rylune@gmail.com>
2022-03-14 02:28:01 -0400

    1.2.0

--- EnhancedOSP/EnhancedOSP.cs
+++ EnhancedOSP/EnhancedOSP.cs
@@ -1,24 +1,25 @@
 ﻿using BepInEx;
 using BepInEx.Configuration;
-using IL.RoR2;
 using Mono.Cecil.Cil;
 using MonoMod.Cil;
 using System;
 using UnityEngine;
+using RoR2;
 
 namespace EnhancedOSP
 {
-    [BepInPlugin("com.Varna.EnhancedOSP", "EnhancedOSP", "1.1.0")]
+    [BepInPlugin("com.Varna.EnhancedOSP", "EnhancedOSP", "1.2.0")]
     public class EnhancedOSP : BaseUnityPlugin
     {
         private static ConfigEntry<float> invTime;
         private static ConfigEntry<float> invThreshold;
         private static ConfigEntry<bool> curseAffectsOSP;
+        private static ConfigEntry<bool> shieldAffectsOSP;
 
         public void Awake()
         {
             invTime = Config.Bind("", "Invulnerable Time", 0.5f, new ConfigDescription("The amount of time a player remains invulnerable after one shot protection is triggered. Vanilla is 0.1"));
-            HealthComponent.TriggerOneShotProtection += (il) =>
+            IL.RoR2.HealthComponent.TriggerOneShotProtection += (il) =>
             {
                 ILCursor c = new ILCursor(il);
                 c.GotoNext(
@@ -32,7 +33,7 @@ namespace EnhancedOSP
             
             invThreshold = Config.Bind("", "OSP Threshold", 0.1f, new ConfigDescription("How much missing hp% before OSP is disabled. Vanilla is 0.1"));
             curseAffectsOSP = Config.Bind("", "Curse Behavior", false, new ConfigDescription("Whether or not sources of MaxHP reduction (Shaped Glass, etc) remove OSP. Vanilla is true"));
-            CharacterBody.RecalculateStats += (il) =>
+            IL.RoR2.CharacterBody.RecalculateStats += (il) =>
             {
 
                 ILCursor c = new ILCursor(il);
@@ -54,6 +55,22 @@ namespace EnhancedOSP
                 c.EmitDelegate<Func<float, RoR2.CharacterBody, float>>((origFrac, body) => { return curseAffectsOSP.Value ?  origFrac : body.oneShotProtectionFraction; });
                 Logger.LogDebug("Curse behavior successful");
             };
+
+            shieldAffectsOSP = Config.Bind("", "Shield Behavior", false, new ConfigDescription("Whether or not sources of Shield (Personal Shield Generator, Overloading affix, etc) count toward your maximum HP for OSP calculations. Vanilla is true [Note: Trancendence and Perfected Elite Affix behavior unaffected to avoid godmode issues]"));
+            IL.RoR2.HealthComponent.TakeDamage += (il) =>
+            {
+                ILCursor c = new ILCursor(il);
+                c.GotoNext(
+                    x => x.MatchLdarg(0),
+                    x => x.MatchCallOrCallvirt<RoR2.HealthComponent>("get_fullCombinedHealth"),
+                    x => x.MatchLdarg(0),
+                    x => x.MatchLdfld<RoR2.HealthComponent>("barrier")
+                    );
+                c.Index++;
+                c.Remove();
+                c.EmitDelegate<Func<RoR2.HealthComponent, float>>((hc) => { return shieldAffectsOSP.Value || hc.body.inventory.GetItemCount(RoR2Content.Items.ShieldOnly) > 0 || hc.body.HasBuff(RoR2Content.Buffs.AffixLunar) ? hc.fullCombinedHealth : hc.fullHealth; });
+            };
+            Logger.LogDebug("Shield behavior successful");
         }
     }
 }
--- EnhancedOSP/README.md
+++ EnhancedOSP/README.md
@@ -5,6 +5,7 @@ A simple mod that allows you to:
 - Set the invincibility window granted by OSP. (default setting is 0.5s, Vanilla is 0.1s)
 - Set the threshold of missing HP required before a player loses OSP. (default is 0.1, or 10% missing HP, same as Vanilla)
 - Set whether sources of maxHP reduction such as Shaped Glass negatively impact OSP. (default is false, Vanilla is true)
+- Set whether sources of Shield such as Personal Shield Generator or Overloading Elite affix negatively impact OSP. Note: Trancendence and Perfected Elite Affix behavior unaffected to avoid godmode issues. (default is false, Vanilla is true)
 
 You can reach me (Varna) in the [RoR2 modding discord](https://discord.gg/5MbXZvd) with any feedback!
 
@@ -12,7 +13,11 @@ You can reach me (Varna) in the [RoR2 modding discord](https://discord.gg/5MbXZv
 
 [ThinkInvis](https://github.com/ThinkInvis/RoR2-TinkersSatchel) - For the location of the curse fraction setting + code I based my settings on.
 
+[RoR2 modding discord](https://discord.gg/5MbXZvd) - Because IL is never without massive headaches.
+
 ## Changelog
+
+1.2.0 - Added setting to control whether Shield affects OSP.
 
 1.1.0 - Added two settings - one to control OSP threshold, and one to control whether maxHP reduction affects OSP (default false).
 

507ce148ad13b97e2980ad3584005b5b61092f5f
varna <rylune@gmail.com>
2022-05-06 19:41:33 -0400

    AllTheTasques 1.0.0

---
+++ AllTheTasques/AllTheTasques.cs
@@ -0,0 +1,49 @@
+﻿using BepInEx;
+using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using MonoMod.RuntimeDetour.HookGen;
+using R2API.Utils;
+using System;
+using System.Reflection;
+
+namespace AllTheTasques
+{
+	[NetworkCompatibility(CompatibilityLevel.NoNeedForSync)]
+	[BepInDependency("com.bepis.r2api")]
+	[BepInDependency("com.Loafe.NSFWTasque")]
+	[BepInPlugin("com.VarnaScelestus.AllTheTasques", "AllTheTasques", "1.0.0")]
+	public class AllTheTasques : BaseUnityPlugin
+	{
+
+		private static readonly MethodInfo m_Tasque = typeof(NSFWTasque.NSFWTasquePlugin).GetMethod("BodyCatalogInit", BindingFlags.Static | BindingFlags.Instance | BindingFlags.NonPublic);
+
+		public void Awake()
+		{
+			HookEndpointManager.Modify(m_Tasque, (Action<ILContext>)AddAllSkins);
+		}
+
+        private void AddAllSkins(ILContext il)
+        {
+			var c = new ILCursor(il);
+			c.Index += 13;
+            while (c.Next != null && c.Index < c.Instrs.Count - 7)
+            {
+				if ( c.Next.OpCode != OpCodes.Call )
+                {
+					c.Remove();
+					//c.Next.OpCode = OpCodes.Nop;
+					//c.Next.Operand = null;
+                }
+				else if (!c.Next.Operand.ToString().Contains("AddMage"))
+				{
+					c.Remove();
+					//c.Next.OpCode = OpCodes.Nop;
+					//c.Next.Operand = null;
+				}
+				else { c.Index++; }
+
+            }
+			//Logger.LogDebug(il.ToString());
+		}
+    }
+}
---
+++ AllTheTasques/README.md
@@ -0,0 +1,13 @@
+# ALL THE TASQUES
+
+Simply adds all the different variants of [NSFWTasque](https://thunderstore.io/package/TheBreadMan/NSFWTasque/) to your character select, instead of just one. Using [ScrollableLobbyUI](https://thunderstore.io/package/KingEnderBrine/ScrollableLobbyUI/) highly recommended.
+
+The original author of [NSFWTasque](https://thunderstore.io/package/TheBreadMan/NSFWTasque/) is hard to find, so I made this small mod to override his configs. It took me four hours because IL is a bunch of bullshit. This will absolutely break if the author ever updates their mod. Don't @ me if/when that happens. Make the bread man add better configs instead.
+
+## Credits
+
+@AllMightyLoaf [NSFWTasque](https://thunderstore.io/package/TheBreadMan/NSFWTasque/) 
+
+## Changelog
+
+1.0.0 - Initial release
